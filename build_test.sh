f0da9310973ac42c39401d59e5e5815ad73b0b3c
李小东 <1658011573@qq.com>
2025-10-17 18:28:15 +0800

    Add files via upload

--- build_test.sh
+++ build_test.sh
@@ -55,8 +55,8 @@ echo "CCACHE_DIR: [$CCACHE_DIR]"
 
 
 MAKE_ARGS="ARCH=arm64 SUBARCH=arm64 O=out LLVM=1 AR=llvm-ar NM=llvm-nm STRIP=llvm-strip OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump HOSTAR=llvm-ar"
-set_CC="ccache clang -Os -ffunction-sections -fdata-sections --target=aarch64-unknown-linux-musl -march=armv8.2-a+lse+crypto+dotprod -mcpu=cortex-a77 -flto=thin -Wno-error -I$PWD/clang/include/aarch64-unknown-linux-musl"
-set_LD="ld.lld --strip-debug -L$PWD/clang/lib/aarch64-unknown-linux-musl"
+set_CC="ccache clang -Os -ffunction-sections -fdata-sections --target=aarch64-unknown-linux-musl -march=armv8.2-a+lse+crypto+dotprod -mcpu=cortex-a77 -flto=thin -Wno-error"
+set_LD="ld.lld --strip-debug"
 set_LDFLAGS_vmlinux="--gc-sections"
 
 
